ee5cc52322a36ceaf78084ec451018901a9b82bf
salmaym <salmaym@banquemisr.com>
2024-04-01 20:28:25 +0200

    feat: implementing sticky navbar

--- scripts/script.js
+++ scripts/script.js
@@ -3,6 +3,7 @@ const menuBtn = document.querySelector('ion-icon[name = "menu-outline"]');
 const header = document.querySelector("header");
 const year = document.querySelector(".year");
 const allLinks = document.querySelectorAll("a");
+const sectionHero = document.querySelector(".hero-section");
 
 function openMenu() {
   header.classList.add("nav-open");
@@ -31,3 +32,21 @@ closeBtn.addEventListener("click", () => {
 window.addEventListener("load", () => {
   year.textContent = new Date().getFullYear();
 });
+
+// sticky nav
+let obs = new IntersectionObserver(
+  function (entries) {
+    let ent = entries[0];
+    if (!ent.isIntersecting) {
+      document.body.classList.add("sticky");
+    } else {
+      document.body.classList.remove("sticky");
+    }
+  },
+  {
+    root: null,
+    threshold: 0,
+    rootMargin: "-80px",
+  }
+);
+obs.observe(sectionHero);
